1e5d188dc22f612906f8b1151f44be28879d5da6
Artyom Pervukhin <artyom@evasive.ru>
2014-06-05 19:09:43 +0400

    mio.Reader added

---
+++ mio_reader.go
@@ -0,0 +1,54 @@
+package mio
+
+import (
+	"io"
+	"time"
+)
+
+// Reader wraps io.Reader and registers each read operation latency in attached
+// histogram
+type Reader struct {
+	io.Reader
+	h Histogram
+}
+
+// NewReader attaches provided histogram to reader, returning new
+// io.Reader. If histogram implements Registrar interface, this would also call
+// its Register() method.
+func NewReader(reader io.Reader, h Histogram) *Reader {
+	mr := &Reader{
+		Reader: reader,
+		h:      h,
+	}
+	if r, ok := h.(Registrar); ok {
+		r.Register()
+	}
+	return mr
+}
+
+// Read implements io.Reader interface; each read operation is timed and sampled
+// in attached histogram. Samples are stored in nanoseconds.
+func (mr *Reader) Read(p []byte) (n int, err error) {
+	var start time.Time
+	if mr.h != nil {
+		start = time.Now()
+	}
+	n, err = mr.Reader.Read(p)
+	if n > 0 && mr.h != nil {
+		mr.h.Update(time.Now().Sub(start).Nanoseconds())
+	}
+	return n, err
+}
+
+// Close implements io.Closer interface. If underlying reader implements
+// io.Closer, calling this method would also close it. If attached histogram
+// also implements Registrar interface, this would call its Done() method.
+func (mr *Reader) Close() error {
+	if r, ok := mr.h.(Registrar); ok {
+		r.Done()
+	}
+	if c, ok := mr.Reader.(io.Closer); ok {
+		return c.Close()
+	}
+	return nil
+}
---
+++ mio_reader_test.go
@@ -0,0 +1,120 @@
+package mio
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/artyom/metrics"
+)
+
+func BenchmarkRawReader(b *testing.B) {
+	file, err := os.Open(os.Args[0])
+	if err != nil {
+		b.Fatal("failed to open file:", err)
+	}
+	defer file.Close()
+	fi, err := file.Stat()
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.SetBytes(fi.Size())
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		if _, err := io.Copy(ioutil.Discard, file); err != nil {
+			b.Fatal("failed to copy data:", err)
+		}
+		if _, err := file.Seek(0, os.SEEK_SET); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkMioReader(b *testing.B) {
+	histogram := metrics.NewHistogram(metrics.NewUniformSample(100))
+	file, err := os.Open(os.Args[0])
+	if err != nil {
+		b.Fatal("failed to open file:", err)
+	}
+	defer file.Close()
+	fi, err := file.Stat()
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.SetBytes(fi.Size())
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		mr := NewReader(file, histogram)
+		if _, err := io.Copy(ioutil.Discard, mr); err != nil {
+			b.Fatal("failed to copy data:", err)
+		}
+		if _, err := file.Seek(0, os.SEEK_SET); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func TestReaderBasic(t *testing.T) {
+	histogram := metrics.NewHistogram(metrics.NewUniformSample(100))
+	file, err := os.Open(os.Args[0])
+	if err != nil {
+		t.Fatal("failed to open file:", err)
+	}
+	defer file.Close()
+	r := io.LimitReader(file, 1<<19)
+	mr := NewReader(r, histogram)
+	n, err := io.Copy(ioutil.Discard, mr)
+	if err != nil {
+		t.Fatal("failed to copy data:", err)
+	}
+	t.Log("bytes copied:", n)
+	t.Logf("%d reads, latency min: %s, max: %s",
+		histogram.Count(),
+		time.Duration(histogram.Min()),
+		time.Duration(histogram.Max()))
+	if histogram.Count() == 0 {
+		t.Fatal("histogram should have some registered samples")
+	}
+}
+
+func TestReaderSelfCleaning(t *testing.T) {
+	histogram := NewSelfCleaningHistogram(
+		metrics.NewHistogram(metrics.NewUniformSample(100)),
+		150*time.Millisecond)
+	file, err := os.Open(os.Args[0])
+	if err != nil {
+		t.Fatal("failed to open file:", err)
+	}
+	defer file.Close()
+	r := io.LimitReader(file, 1<<19)
+	mr := NewReader(r, histogram)
+	n, err := io.Copy(ioutil.Discard, mr)
+	if err != nil {
+		t.Fatal("failed to copy data:", err)
+	}
+	if err := mr.Close(); err != nil {
+		t.Fatal("metered reader close error:", err)
+	}
+	t.Log("bytes copied:", n)
+	t.Logf("%d reads, latency min: %s, max: %s",
+		histogram.Count(),
+		time.Duration(histogram.Min()),
+		time.Duration(histogram.Max()))
+	if histogram.Count() == 0 {
+		t.Fatal("histogram should have some registered samples")
+	}
+	t.Log("waiting for released histogram to clear")
+	time.Sleep(200 * time.Millisecond)
+	cnt := histogram.Count()
+	t.Logf("%d writes, latency min: %s, max: %s",
+		cnt,
+		time.Duration(histogram.Min()),
+		time.Duration(histogram.Max()))
+	if cnt != 0 {
+		t.Fatal("histogram should be empty, but has samples:", cnt)
+	}
+}
